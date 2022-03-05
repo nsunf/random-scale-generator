37894249b19c15c46df695f1cb98cd9dbf945cde
nsunf <jun5013@naver.com>
2022-03-05 15:23:26 +0900

    edit port number to .env.PORT

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("server is running");
 });
